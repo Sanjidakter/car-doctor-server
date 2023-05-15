beffe9be64a930a2e68eb20632a691df41b456c3
Sanjida Akter <sanjidakter2304@gmail.com>
2023-05-15 19:46:55 +0600

    token access verification in jwt

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,6 +24,24 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req,res,next) => {
+console.log('hitting verify JWT')
+console.log(req.headers.authorization);
+const authorization = req.headers.authorization;
+if(!authorization){
+  return res.status(401).send({error:true , message: 'unauthorized access'})
+}
+const token = authorization.split(' ')[1];
+console.log('token inside verify JWT',token);
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+  if(error){
+    return res.status(403).send({error:true, message:'unauthorized access'})
+  }
+  req.decoded = decoded;
+  next();
+})
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,17 +50,43 @@ async function run() {
     const serviceCollection = client.db("carDoctorDb").collection("services");
     const bookingCollection = client.db("carDoctorDb").collection("bookings");
 
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
+
+    // services routes
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    // bookings
-    app.get("/bookings", async (req, res) => {
-      console.log(req.query.email)
-      let query ={};
-      if(req.query?.email){
-        query = {email:req.query.email}
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    
+    // bookings routes
+    app.get("/bookings",verifyJWT, async (req, res) => {
+      console.log('came back after verify');
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
       }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
@@ -54,39 +99,27 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/bookings/:id', async(req,res) => {
-      const id  = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
       console.log(updatedBooking);
       const updateDoc = {
-        $set : {
-          status : updatedBooking.status
+        $set: {
+          status: updatedBooking.status,
         },
       };
-      const result = await bookingCollection.updateOne(filter,updateDoc);
+      const result = await bookingCollection.updateOne(filter, updateDoc);
       res.send(result);
-    })
+    });
 
-    app.delete('/bookings/:id',async(req,res)=>{
+    app.delete("/bookings/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-
-            const options = {
-                // Include only the `title` and `imdb` fields in the returned document
-                projection: { title: 1, price: 1, service_id: 1, img: 1 },
-            };
-
-            const result = await serviceCollection.findOne(query, options);
-            res.send(result);
-        })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
